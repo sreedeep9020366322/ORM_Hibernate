32bd5643819be7f0b12670f868405205bc9e6433
sperin <sperin@delbahamas41048.sapient.com>
2016-03-18 15:03:29 +0530

    new update

--- ORM_With_Hibernate/src/com/training/apps/one2ManyApplication.java
+++ ORM_With_Hibernate/src/com/training/apps/one2ManyApplication.java
@@ -9,6 +9,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
+import com.training.entity.Address;
 import com.training.entity.ClinicalAssistant;
 import com.training.entity.Doctor;
 import com.training.entity.InPatient;
@@ -23,15 +24,15 @@ public class one2ManyApplication {
 		
 		Session session = factory.openSession();
 		Transaction tx = session.beginTransaction();
-		Doctor doc = new Doctor(7212, "LatestDoc", 8997);
-		ClinicalAssistant assistant = new ClinicalAssistant(02002, "Assistant1");
+		Doctor doc = new Doctor(3, "LatestDoc", 8997);
+		ClinicalAssistant assistant = new ClinicalAssistant(22, "Assistant1");
 		
 		Timestamp date = new Timestamp(new Date().getTime());
 		
 		Set<Patient> patientList = new HashSet<Patient>();
-		InPatient patient1 = new InPatient(050,"inpatient1",21,600,date);
-		InPatient patient2 = new InPatient(051,"inpatient2",22,601,date);
-		InPatient patient3 = new InPatient(052,"inpatient3",23,602,date);
+		InPatient patient1 = new InPatient(1,"inpatient",21,600,date);
+		InPatient patient2 = new InPatient(2,"inpatient",22,601,date);
+		InPatient patient3 = new InPatient(3,"inpatient",23,602,date);
 		
 		patientList.add(patient1);
 		patientList.add(patient2);
@@ -40,9 +41,18 @@ public class one2ManyApplication {
 		doc.setAssistant(assistant);
 		doc.setPatientList(patientList);
 		
+		Address cliniAddress = new Address("sector 21", "pocket E", "Gurgaon", 122002);
+		Address resiAddress = new Address("Sector 23", "Pocket W", "City 1", 5445);
+		
+		doc.setClinicalAddress(cliniAddress);
+		doc.setResidentialAddress(resiAddress);
+		
+		
 		session.save(doc);
 		tx.commit();
 		session.close();
+		
+		factory.close();
 		
 	}
 }
